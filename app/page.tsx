a32767d80ff44d590badc45a091ee6038b0fd6c8
qmqbank <qmqbank25no@gmail.com>
2025-08-20 16:46:06 +0600

    Added a new button in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
+export default function Home() {
   return (
-    <h1 className="text-5xl text-red-500">
-      🚀 Tailwind v3 Working!
-    </h1>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6">
+        Welcome Madrasah-Question-Bank
+      </h1>
+      
+      <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg shadow-md transition">
+        Click Me
+      </button>
+    </main>
+  );
 }
